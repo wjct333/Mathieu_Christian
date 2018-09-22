29272492af2629ccdb13ef927eeb5f65b6f70eb0
asus <wjct333@gmail.com>
2018-09-22 22:33:21 +1000

    Add Exercise

---
+++ Exercises.py
@@ -0,0 +1,55 @@
+
+# Welcome to the Week 7 CodeBench exercise set. As with the Week 6 set, we start with some revision exercises, and then have some slightly longer algorithm design questions to finish.
+#
+# The code below defines a Person class. Each person has a name and a list of friends that other pople can be added to.
+#
+# You jobs are to:
+#
+# Implement the constructor for person so that the name given to the constructor is stored in a member variable called name and a member variable called friends is initialised to an empty list.
+#
+# Implement the method is_friends_with. If a and b are people then this method should return true if a has been added as a friend of b or if b has been added as a friend of a.
+#
+# Implement the add_friend method. If a and b are instances of Person then after a.add_friend(b) is called a must be a friend of b and b must be a friend of a.
+#
+# The tests at the end of the program will check whether all the above conditions are properly implemented by your code.
+#
+# An exercise on Classes, lecture 12.
+
+
+class Person:
+
+    def __init__(self, name):
+        """Create a new person with the given name and no friends."""
+
+    # TODO: Implement this
+
+    def add_friend(self, other):
+        """Make this person and the given other person friends."""
+
+    # TODO: Implement this.
+
+    def is_friends_with(self, other):
+        """Returns True if and only if other is a friend of this person."""
+        # TODO: Fix this
+        return False
+
+
+# DO NOT MODIFY CODE BELOW THIS LINE
+import unittest
+
+
+class FriendTests(unittest.TestCase):
+
+    def test_init(self):
+        alice = Person('Alice')
+        self.assertEqual(alice.name, 'Alice')
+        self.assertEqual(alice.friends, [])
+
+    def test_add(self):
+        alice = Person('Alice')
+        bob = Person('Bob')
+        alice.add_friend(bob)
+        self.assertTrue(alice.is_friends_with(bob))
+        self.assertTrue(bob.is_friends_with(alice))
+
+
